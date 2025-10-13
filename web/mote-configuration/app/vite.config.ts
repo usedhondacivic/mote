a0777f5985a5c2bb4ac93bcee8f8fa34fd05ed6c
usedhondacivic <micacer48@gmail.com>
2025-10-13 15:16:48 -0700

    Use existing workflow for book install

--- web/mote-configuration/app/vite.config.ts
+++ web/mote-configuration/app/vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/mote/configuration",
+    base: "/mote/configuration/",
     plugins: [svelte()],
     server: {
         fs: {
