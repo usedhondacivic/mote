3b64cf35643b1a8b3d77b5ee62ae5949a407fc67
usedhondacivic <micacer48@gmail.com>
2026-03-07 12:48:41 -0800

    Cleanup

--- mote-ffi/mote_link/__main__.py
+++ mote-ffi/mote_link/__main__.py
@@ -6,7 +6,7 @@ from mote_link.link import MoteClient
 # Example application that moves the robot using the keyboard and logs sensor data to the console.
 async def run_main():
     async with MoteClient() as client:
-        await client.connect_with_uid("mote")
+        await client.connect_with_uid("mote-:3")
 
 
 if __name__ == "__main__":
--- mote-ffi/mote_link/link.py
+++ mote-ffi/mote_link/link.py
@@ -1,6 +1,9 @@
+# PyO3 does not support exporting type stubs for generated modules
+# https://github.com/PyO3/maturin/pull/2940
+import mote_link.mote_ffi  # ty:ignore[unresolved-import]
+
 from zeroconf.asyncio import AsyncZeroconf
 from zeroconf import Zeroconf, IPVersion, AddressResolver
-import mote_link.mote_ffi
 
 import asyncio
 import ipaddress
@@ -8,7 +11,7 @@ import ipaddress
 import socket
 
 
-async def get_ip_from_mdns(service_name):
+async def chose_from_mdns_service(service_name):
     return None
 
 
@@ -30,13 +33,13 @@ class MoteClient:
         This method will open an interactive discovery prompt.
         Use this method if you do not know the ip or unique ID of your robot and your network supports MDNS.
         """
-        await get_ip_from_mdns("Mote Command Server")
+        await chose_from_mdns_service("_mote-api._udp.local.")
 
     async def connect_with_uid(self, uid: str):
         """
         Connect to Mote.
 
-        Use this method if you know the unique ID of your robot, and your network supports MDNS.
+        Use this method if you know the unique ID of your robot, and your network / OS support MDNS.
         """
         try:
             # This will use the underlying OS resolution mechanism
@@ -45,6 +48,9 @@ class MoteClient:
             print(f"Resolved IP address for {hostname}: {self.ip}")
         except socket.error as e:
             print(f"Error resolving {hostname}: {e}")
+            print(
+                "Did you use the correct uid? Is Mote connected to your network? Does your network support mdns?"
+            )
 
     async def connect_with_ip(self, ip: ipaddress.IPv4Address):
         """
